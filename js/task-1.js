1ef364d9c6a07a712d082d8648439a8889d5c6af
EmilianWieczorek <155941604+EmilianWieczorek@users.noreply.github.com>
2024-05-07 06:55:30 +0200

    save-01

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,27 @@
-function makeTransaction(quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid, totalCredits) {
   const totalPrice = quantity * pricePerDroid;
-  console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+  //console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+
+  if (totalPrice <= totalCredits) {
+    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+  } else {
+    console.log(`Insufficient funds!`);
+  }
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+// 1.1 Zmienne i typy. Podstawa funkcji //
+
+// function makeTransaction(quantity, pricePerDroid) {
+//   const totalPrice = quantity * pricePerDroid;
+//   console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+// }
+
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,14 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = Number.parseFloat(content);
-  const paddingValue = Number.parseFloat(padding);
-  const borderValue = Number.parseFloat(border);
+// function getElementWidth(content, padding, border) {
+//   const contentValue = Number.parseFloat(content);
+//   const paddingValue = Number.parseFloat(padding);
+//   const borderValue = Number.parseFloat(border);
 
-  const width = contentValue + paddingValue * 2 + borderValue * 2;
+//   const width = contentValue + paddingValue * 2 + borderValue * 2;
 
-  //return contentValue + paddingValue * 2 + borderValue * 2;
-  console.log(`${width}`);
-}
+//   //return contentValue + paddingValue * 2 + borderValue * 2;
+//   console.log(`${width}`);
+// }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
